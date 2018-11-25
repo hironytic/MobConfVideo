f4806d95d522b991379452c29e37990a3f08a72a
Hironori Ichimiya <hiron@hironytic.com>
2018-11-25 20:37:45 +0900

    Building request UI

--- lib/request/request_page.dart
+++ lib/request/request_page.dart
@@ -14,16 +14,35 @@ class _RequestPageState extends State<RequestPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("リクエスト"),
+//        title: Text("リクエスト"),
+          title: PopupMenuButton(
+            itemBuilder: (BuildContext context) =>
+            <PopupMenuEntry<int>>[
+              const PopupMenuItem(value: 0, child: Text("第2回 カンファレンス動画鑑賞会")),
+              const PopupMenuItem(value: 1, child: Text("第1回 カンファレンス動画鑑賞会")),
+              const PopupMenuItem(value: 1, child: Text("第0回 カンファレンス動画鑑賞会")),
+            ],
+            child: Text("第0回 カンファレンス動画鑑賞会 \u25be"),
+          )
       ),
       body: ListView(
         children: <Widget>[
           ListTile(
-            title: Text("hogehoge"),
+            title: Text("スマホアプリエンジニアだから知ってほしいブロックチェーンと分散型アプリケーション"),
+            subtitle: Text("iOSDC Japan 2018"),
+            trailing: Icon(Icons.check),
           ),
           Divider(),
           ListTile(
-            title: Text("foo bar"),
+            title: Text("DDD(ドメイン駆動設計)を知っていますか？？"),
+            subtitle: Text("iOSDC 2018 Reject Conference"),
+            trailing: Icon(Icons.check),
+          ),
+          Divider(),
+          ListTile(
+            title: Text("再利用可能なUI Componentsを利用したアプリ開発"),
+            subtitle: Text("iOSDC Japan 2018"),
+            trailing: null,
           ),
           Divider(),
         ],
